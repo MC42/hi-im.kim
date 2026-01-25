4618ca6d9b602f5586a9e01ecfc223c40f30e9d7
Kim <MC42@users.noreply.github.com>
2026-01-24 18:42:28 -0600

    remove toc

--- writing/pmmm/and_thus_the_heavens_wept/build.sh
+++ writing/pmmm/and_thus_the_heavens_wept/build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 for file_name in ./*.md ; do
-    pandoc $file_name -s --metadata author="Kim Flores" --toc --strip-comments --wrap=none -o "${file_name%.md}.html" 2>/dev/null
+    pandoc $file_name -s --metadata author="Kim Flores" --strip-comments --wrap=none -o "${file_name%.md}.html" 2>/dev/null
     tempura=$(pandoc $file_name --strip-comments -t markdown | wc -w)
     echo $file_name    $tempura
 done
